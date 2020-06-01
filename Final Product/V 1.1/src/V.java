f187c2a4e6ab8a900f491be7431906bfdd11791a
AvengingPrime <32879528+AvengingPrime@users.noreply.github.com>
2020-06-01 03:35:15 -0500

    Add files via upload

---
+++ Final Product/V 1.1/src/V.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class V
+{
+	VCompiler compiler;
+	GUI gui;
+	
+	public V()
+	{
+		gui = new GUI(this);
+		compiler = null;
+		run();
+	}
+	
+	public void run()
+	{
+		while(true)
+		{
+//			System.out.println("WORKS");
+			if(gui.run)
+			{
+				System.out.println("WORKS");
+				compile(gui.save());
+			}
+		}
+	}
+	
+	public void compile(File file)
+	{
+		System.out.println("Working");
+		compiler = new VCompiler(file);
+//		compiler.interpret();
+	}
+	
+	public static void main(String[] args)
+	{
+		V v = new V();
+	}
+	
+	
+}
